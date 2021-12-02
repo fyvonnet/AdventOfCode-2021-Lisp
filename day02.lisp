7275008c0ad85885cdea13a2e332a0ae9b1af059
Franck YVONNET <franck@yvonnet.fr>
2021-12-02 07:19:56 +0100

    refactoring

--- day02.lisp
+++ day02.lisp
@@ -9,31 +9,33 @@
   (destructuring-bind (cmd x) (split " " line)
     (cons (intern cmd "KEYWORD") (parse-integer x))))
 
-(defun navigate (commands &optional (hpos 0) (depth 0))
-  (if (null commands)
-    (* hpos depth)
-    (destructuring-bind (cmd . x) (car commands)
-      (destructuring-bind (new-hpos new-depth)
-        (case cmd 
-          (:|forward| (list (+ hpos x) depth))
-          (:|down| (list hpos (+ depth x)))
-          (:|up| (list hpos (- depth x))))
-        (navigate (cdr commands) new-hpos new-depth)))))
-
-(defun navigate2 (commands &optional (hpos 0) (depth 0) (aim 0))
-  (if (null commands)
-    (* hpos depth)
-    (destructuring-bind (cmd . x) (car commands)
-      (destructuring-bind (new-hpos new-depth new-aim)
-        (case cmd 
-          (:|down| (list hpos depth (+ aim x)))
-          (:|up| (list hpos depth (- aim x)))
-          (:|forward| (list (+ hpos x) (+ depth (* aim x)) aim)))
-        (navigate2 (cdr commands) new-hpos new-depth new-aim)))))
+(defun solve (input forw-func down-func up-func)
+  (destructuring-bind (h d _)
+    (reduce
+      (lambda (data cmd)
+        (destructuring-bind (h d a) data
+          (funcall
+            (case (car cmd)
+              (:|forward| forw-func)
+              (:|down|    down-func)
+              (:|up|        up-func))
+            h d a (cdr cmd))))
+      input :initial-value '(0 0 0))
+    (print (* h d))))
 
 (defun main ()
   (let
     ((input (read-input-as-list 02 #'decode)))
-    (print (navigate  input))
-    (print (navigate2 input))))
+
+    (solve 
+      input
+      (lambda (h d a x) (list (+ h x) d    0))
+      (lambda (h d a x) (list    h (+ d x) 0))
+      (lambda (h d a x) (list    h (- d x) 0)))
+
+    (solve 
+      input
+      (lambda (h d a x) (list (+ h x) (+ d (* a x))    a   ))
+      (lambda (h d a x) (list    h       d          (+ a x)))
+      (lambda (h d a x) (list    h       d          (- a x))))))
 
