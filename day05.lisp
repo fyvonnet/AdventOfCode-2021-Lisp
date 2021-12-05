497ad5442c72bc01c87775723723b6ab16fa97fc
Franck YVONNET <franck@yvonnet.fr>
2021-12-05 18:24:51 +0100

    use one two-layered diagram

--- day05.lisp
+++ day05.lisp
@@ -17,9 +17,7 @@
       ((> a b) (seq (1- a) b))))) 
 
 (defun main ()
-  (let
-    ((diagram1 (make-array '(1000 1000) :initial-element 0))
-     (diagram2 (make-array '(1000 1000) :initial-element 0)))
+  (let ((diagram (make-array '(2 1000 1000) :initial-element 0)))
 
     (iterate
       (for s in (read-input-as-list 5 #'decode))
@@ -28,25 +26,25 @@
           ((= x1 x2)
            (iterate
              (for y in (seq y1 y2))
-             (incf (aref diagram1 y x1))
-             (incf (aref diagram2 y x1))))
+             (incf (aref diagram 0 y x1))
+             (incf (aref diagram 1 y x1))))
           ((= y1 y2)
            (iterate
              (for x in (seq x1 x2))
-             (incf (aref diagram1 y1 x))
-             (incf (aref diagram2 y1 x))))
+             (incf (aref diagram 0 y1 x))
+             (incf (aref diagram 1 y1 x))))
           (t
             (iterate 
               (for y in (seq y1 y2))
               (for x in (seq x1 x2))
-              (incf (aref diagram2 y x)))))))
+              (incf (aref diagram 1 y x)))))))
 
-    (dolist (d `(,diagram1 ,diagram2))
+    (dolist (l '(0 1))
       (print
         (iterate
           (for y below 1000)
           (sum
             (iterate
               (for x below 1000)
-              (counting (< 1 (aref d y x))))))))))
+              (counting (< 1 (aref diagram l y x))))))))))
 
