ebd2af427c11608441881b05024c70dfcf71efc3
Franck YVONNET <franck@yvonnet.fr>
2021-12-05 08:30:34 +0100

    moved draw-segment and count-overlaps in main

--- day05.lisp
+++ day05.lisp
@@ -16,41 +16,37 @@
       ((< a b) (seq (1+ a) b))
       ((> a b) (seq (1- a) b))))) 
 
-(defun draw-segment (diagram1 diagram2)
-  (lambda (segment)
-    (destructuring-bind (x1 y1 x2 y2) segment
-      (cond
-        ((= x1 x2)
-         (iterate
-           (for y in (seq y1 y2))
-           (incf (aref diagram1 y x1))
-           (incf (aref diagram2 y x1))))
-        ((= y1 y2)
-         (iterate
-           (for x in (seq x1 x2))
-           (incf (aref diagram1 y1 x))
-           (incf (aref diagram2 y1 x))))
-        (t
-          (iterate 
-            (for y in (seq y1 y2))
-            (for x in (seq x1 x2))
-            (incf (aref diagram2 y x))))))))
-
-(defun count-overlaps (diagram)
-  (iterate
-    (for y below 1000)
-    (sum
-      (iterate
-        (for x below 1000)
-        (counting (< 1 (aref diagram y x)))))))
-
 (defun main ()
-  (let*
-    ((segments (read-input-as-list 5 #'decode))
-     (diagram1 (make-array '(1000 1000) :initial-element 0))
+  (let
+    ((diagram1 (make-array '(1000 1000) :initial-element 0))
      (diagram2 (make-array '(1000 1000) :initial-element 0)))
 
-    (mapcar (draw-segment diagram1 diagram2) segments)
-    (print (count-overlaps diagram1))
-    (print (count-overlaps diagram2))))
+    (iterate
+      (for s in (read-input-as-list 5 #'decode))
+      (destructuring-bind (x1 y1 x2 y2) s
+        (cond
+          ((= x1 x2)
+           (iterate
+             (for y in (seq y1 y2))
+             (incf (aref diagram1 y x1))
+             (incf (aref diagram2 y x1))))
+          ((= y1 y2)
+           (iterate
+             (for x in (seq x1 x2))
+             (incf (aref diagram1 y1 x))
+             (incf (aref diagram2 y1 x))))
+          (t
+            (iterate 
+              (for y in (seq y1 y2))
+              (for x in (seq x1 x2))
+              (incf (aref diagram2 y x)))))))
+
+    (dolist (d `(,diagram1 ,diagram2))
+      (print
+        (iterate
+          (for y below 1000)
+          (sum
+            (iterate
+              (for x below 1000)
+              (counting (< 1 (aref d y x))))))))))
 
