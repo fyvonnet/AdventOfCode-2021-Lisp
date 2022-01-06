94fb571c4580eeb822face97ef8b119edd31935a
Franck YVONNET <franck@yvonnet.fr>
2022-01-06 11:26:56 +0100

    new version

--- day01.lisp
+++ day01.lisp
@@ -1,23 +1,20 @@
 (defpackage :day01
   (:use :cl :aoc-misc)
-  (:export main)
-  (:import-from :forfuncs :for/count))
+  (:import-from :serapeum :nlet)
+  (:export main))
 
 (in-package :day01)
 
-(defun count-increases (measurements)
-  (for/count
-    ((a      measurements)
-     (b (cdr measurements)))
-    (< a b)))
+(defmacro count-increases (dest num1 num2)
+  ` (nlet rec ((measurements input) (count 0))
+      (destructuring-bind (,@dest &rest rest) measurements
+        (let ((new-count (if (< ,num1 ,num2) (1+ count) count)))
+          (if (null rest)
+            (format t "~D~%" new-count)
+            (rec (cdr measurements) new-count))))))
 
 (defun main ()
-  (let ((measurements (read-input-as-list 1 #'parse-integer)))
-    (format t "~D~%" (count-increases measurements))
-    (format t "~D~%"
-            (count-increases
-              (loop for a in       measurements
-                    for b in (cdr  measurements)
-                    for c in (cddr measurements)
-                    collect (+ a b c))))))
+  (let ((input (read-input-as-list 1 #'parse-integer)))
+    (count-increases (a b) a b)
+    (count-increases (a b c d) (+ a b c) (+ b c d))))
 
